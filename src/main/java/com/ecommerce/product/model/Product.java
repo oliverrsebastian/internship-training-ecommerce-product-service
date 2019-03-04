f7cdc773ca16a5a4a0d97182d908653e11927a8d
oliverrsebastian <40243230+oliverrsebastian@users.noreply.github.com>
2019-03-04 18:59:37 +0700

    initial upload

---
+++ src/main/java/com/ecommerce/product/model/Product.java
@@ -0,0 +1,54 @@
+package com.ecommerce.product.model;
+
+public class Product {
+
+    private String id;
+    private String name;
+    private Long price;
+    private Integer qty;
+    private String categoryName;
+
+
+    public Product() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public void setQty(Integer qty) {
+        this.qty = qty;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
